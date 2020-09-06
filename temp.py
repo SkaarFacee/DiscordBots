d3b590e918ca699267bdc93d40755bcd65cc0ed3
adiaux <aditya8anil@gmail.com>
2020-09-06 14:23:51 +0530

    Third commit: Complete basic bot

--- temp.py
+++ temp.py
@@ -1,14 +1,32 @@
 import os
+import random
 
-import discord
-from secret  import DISCORD_TOKEN
+from discord.ext import commands
+from secret  import DISCORD_TOKEN,DISCORD_GUILD
 
 TOKEN = os.getenv(DISCORD_TOKEN)
+bot = commands.Bot(command_prefix='!')
 
-client = discord.Client()
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
 
-client.run(DISCORD_TOKEN)
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice, number_of_sides):
+    dice = [
+        str(random.choice(range(1, int(number_of_sides) + 1)))
+        for _ in range(int(number_of_dice))
+    ]
+    await ctx.send(', '.join(dice))
+
+bot.run(DISCORD_TOKEN)
