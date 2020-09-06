17168f56caa3cbfb6c8ad094b0cd996451366d54
adiaux <aditya8anil@gmail.com>
2020-09-07 04:09:49 +0530

    Fourth: Add suggestions to embed

--- temp.py
+++ temp.py
@@ -1,5 +1,6 @@
 import os
 import random
+import discord 
 
 from discord.ext import commands
 from secret  import DISCORD_TOKEN,DISCORD_GUILD
@@ -21,6 +22,15 @@ async def nine_nine(ctx):
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
 
+@bot.command(name='11', help='Embed text')
+async def create(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}",description="Test am embed")
+    embed.add_field(name=f"Question 1",value=f"Created at :{ctx.guild.created_at}") 
+    panel = await ctx.channel.send(embed=embed)
+    await panel.add_reaction('\U0001F44D')
+    await panel.add_reaction('\U0001F44E')
+
+
 @bot.command(name='roll_dice', help='Simulates rolling dice.')
 async def roll(ctx, number_of_dice, number_of_sides):
     dice = [
@@ -30,3 +40,4 @@ async def roll(ctx, number_of_dice, number_of_sides):
     await ctx.send(', '.join(dice))
 
 bot.run(DISCORD_TOKEN)
+
